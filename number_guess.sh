58a2a7ea8b4558feb310a0f74eea0a0f3fb04966
matt n <mathiasnitz@gmx.de>
2025-03-28 22:15:33 +0000

    fix: refactored a lot of things

--- number_guess.sh
+++ number_guess.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "Enter your username:"
-read USERNAME
-
 #secret number
 SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
 
@@ -10,6 +7,50 @@ NUMBER_OF_GUESSES=0
 
 #datenbankverbindung in psql
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+GUESSING_GAME() {
+while true; 
+do
+
+  read GUESSED_NUMBER
+
+  if [[ $GUESSED_NUMBER =~ ^[0-9]+$ ]]
+  then
+
+    NUMBER_OF_GUESSES=$(( NUMBER_OF_GUESSES + 1 ))
+
+    
+
+    if [[ $SECRET_NUMBER -gt $GUESSED_NUMBER ]]
+    then
+      echo "It's higher than that, guess again:"
+      $PSQL "UPDATE games SET number_of_guesses=$NUMBER_OF_GUESSES WHERE game_id=$GAME_ID" 
+        
+    elif [[ $SECRET_NUMBER -lt $GUESSED_NUMBER ]]
+    then
+      echo "It's lower than that, guess again:"
+      $PSQL "UPDATE games SET number_of_guesses=$NUMBER_OF_GUESSES WHERE game_id=$GAME_ID" 
+
+    else
+
+      echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+
+      $PSQL "UPDATE games SET number_of_guesses=$NUMBER_OF_GUESSES WHERE game_id=$GAME_ID"
+
+      if [[ $NUMBER_OF_GUESSES -lt $BEST_GAME ]]
+      then
+        $PSQL "UPDATE users SET best_game=$NUMBER_OF_GUESSES WHERE username='$USERNAME'"
+      fi
+      break;
+    fi
+  else
+    echo "That is not an integer, guess again:"
+  fi
+done
+}
+
+echo "Enter your username:"
+read USERNAME
 
 if [[ ! -z $USERNAME ]]
 then
@@ -22,7 +63,7 @@ then
     echo "Welcome, $USERNAME! It looks like this is your first time here."
 
     #user zur users tabelle hinzufügen
-    $PSQL "INSERT INTO users(username, games_played, best_game) VALUES('$USERNAME', 1, NULL)"
+    $PSQL "INSERT INTO users(username, games_played, best_game) VALUES('$USERNAME', 1, 9999)"
 
     BEST_GAME=$($PSQL "SELECT best_game FROM users WHERE username ILIKE '$USERNAME'")
 
@@ -30,7 +71,7 @@ then
     USER_ID=$($PSQL "SELECT user_id FROM users WHERE username ILIKE '$USERNAME'") 
 
     #ein neues spiel für diesen user in games anlegen
-    NEW_GAME_ID=$($PSQL "INSERT INTO games(user_id) VALUES($USER_ID)")
+    $PSQL "INSERT INTO games(user_id, number_of_guesses) VALUES($USER_ID, 0)"
 
   else
 
@@ -43,53 +84,12 @@ then
     echo "Welcome back, $USERNAME_DB! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 
     $PSQL "UPDATE users SET games_played=games_played+1 WHERE user_id=$USER_ID"
-    $PSQL "INSERT INTO games(user_id) VALUES($USER_ID)"
+    $PSQL "INSERT INTO games(user_id, number_of_guesses) VALUES($USER_ID, 0)"
   fi
+
+  GAME_ID=$($PSQL "SELECT MAX(game_id) FROM games WHERE user_id=$USER_ID")
 
   echo "Guess the secret number between 1 and 1000:"
+  GUESSING_GAME
 
 fi
-
-GUESSING_GAME() {
-while true; 
-do
-
-  read GUESSED_NUMBER
-
-  if [[ $GUESSED_NUMBER =~ ^[0-9]+$ ]]
-  then
-
-    NUMBER_OF_GUESSES=$((NUMBER_OF_GUESSES + 1))
-
-    GAME_ID=$($PSQL "SELECT game_id FROM games WHERE user_id=$USER_ID ORDER BY game_id DESC LIMIT 1")
-
-    if [[ $SECRET_NUMBER -gt $GUESSED_NUMBER ]]
-    then
-      echo "It's higher than that, guess again:"
-      $PSQL "UPDATE games SET number_of_guesses=$NUMBER_OF_GUESSES WHERE game_id=$GAME_ID" 
-        
-    elif [[ $SECRET_NUMBER -lt $GUESSED_NUMBER ]]
-    then
-      echo "It's lower than that, guess again:"
-      $PSQL "UPDATE games SET number_of_guesses=$NUMBER_OF_GUESSES WHERE game_id=$GAME_ID" 
-
-    elif [[ $SECRET_NUMBER -eq $GUESSED_NUMBER ]]
-    then
-
-      echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
-
-      $PSQL "UPDATE games SET number_of_guesses=$NUMBER_OF_GUESSES WHERE game_id=$GAME_ID"
-
-      if [[ -z $BEST_GAME || $NUMBER_OF_GUESSES -lt $BEST_GAME ]]
-      then
-        $PSQL "UPDATE users SET best_game=$NUMBER_OF_GUESSES WHERE user_id=$USER_ID"
-      fi
-      break;
-    fi
-  else
-    echo "That is not an integer, guess again:"
-  fi
-done
-}
-
-GUESSING_GAME
